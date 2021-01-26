049905494f4730759959904324244b932993fd08
twotendersandasideofranch <sgnaw73@gmail.com>
2021-01-26 04:05:00 -0800

    Lab 4
    Lab 4 Tasks

---
+++ lab4/js/lab.js
@@ -0,0 +1,8 @@
+<pre>
+  / / Establish the set of integers (1, 2, 3, 4, 5, 6)
+  / / Accept user input
+    / / User clicks on whether they want to roll the dice
+  / / Computer chooses a random integers (1,6)
+  / / Displays what number was chosen
+  / / Computer asks if you want to roll again
+</pre>
