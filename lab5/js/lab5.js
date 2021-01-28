569c8f5264232cfde8db11165b47d3648748b96b
twotendersandasideofranch <sgnaw73@gmail.com>
2021-01-27 21:52:33 -0800

    Lab 5
    My completed Lab 5, where we worked with data types and variables. First time working with js!

---
+++ lab5/js/lab5.js
@@ -0,0 +1,27 @@
+/*
+ * Author: Simon Wang <swang246@ucsc.edu>
+ * Created: 27 January
+ * License: Public Domain
+ */
+
+
+// Define Variables
+
+  var make = "Lexus";
+  var model = "ES 330";
+  var color = "Grey";
+  var year = 2004;
+
+// Calculate
+  age = 2021 - year;
+
+// Boolean - Own It
+  var ownIt = true 
+
+// Output
+
+  document.writeln("Make: " + make + "<br>");
+  document.writeln("Model: " + model + "<br>");
+  document.writeln("Color: " + color + "<br>");
+  document.writeln("Year: " + year + "<br>");
+  document.writeln("Age: " + age + " years<br>");
